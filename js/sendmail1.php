2ddd709d71fa941b3243b770900d5b97f906c326
Devang Gandhi <devanggandhi69@gmail.com>
2017-02-06 13:46:37 -0800

    website

---
+++ js/sendmail1.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+$mail = new PHPMailer;
+
+//Enable SMTP debugging. 
+$mail->SMTPDebug = 3;                               
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();            
+//Set SMTP host name                          
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                          
+//Provide username and password     
+$mail->Username = "devanggandhi69@gmail.com";                 
+$mail->Password = "33826434892";                           
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                           
+//Set TCP port to connect to 
+$mail->Port = 587;                                   
+
+$mail->From = "devanggandhi69@gmail.com";
+$mail->FromName = "Full Name";
+
+$mail->addAddress("ndevanggandhi69@gmail.com", "DEvang Gandhi");
+
+$mail->isHTML(true);
+
+$mail->Subject = "A message from Aarisha Dental website";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+if(!$mail->send()) 
+{
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} 
+else 
+{
+    echo "Message has been sent successfully";
+}
